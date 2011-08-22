cb622da4322a61c4c28292f21df0dbee4e3e2678
Drew Neil <andrew.jr.neil@gmail.com>
2011-08-22 16:22:28 +0200

    Use ItemTextTpl to add meta-data for each model.

--- app/views/Viewport.js
+++ app/views/Viewport.js
@@ -15,7 +15,22 @@ NestedListDemo.views.Viewport = Ext.extend(Ext.NestedList, {
         return detailCard;
     },
     getItemTextTpl: function() {
-        var tplConstructor = '{text}';
+        var tplConstructor = '{text}' +
+            '<tpl if="model === \'Artist\'">'+
+                '<div class="metadata">' +
+                    ' {[values.items.length]} albums' +
+                '</div>' +
+            '</tpl>' +
+            '<tpl if="model === \'Album\'">'+
+                '<div class="metadata">' +
+                    ' {[values.items.length]} tracks' +
+                '</div>' +
+            '</tpl>' +
+            '<tpl if="model === \'Track\'">'+
+                '<div class="metadata">' +
+                    ' Duration: {[Math.floor(values.duration/60)]}:{[values.duration%60]}' +
+                '</div>' +
+            '</tpl>';
         return tplConstructor;
     }
 });
