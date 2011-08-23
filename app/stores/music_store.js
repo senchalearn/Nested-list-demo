5d350c640f9a2572d05e13f6ea0b231cedd7f3b0
Drew Neil <andrew.jr.neil@gmail.com>
2011-08-23 16:47:38 +0100

    Use full URL for tracks.json data file.

--- app/stores/music_store.js
+++ app/stores/music_store.js
@@ -2,7 +2,7 @@ NestedListDemo.music_store = new Ext.data.TreeStore({
     model: 'ListItem',
     proxy: {
         type: 'ajax',
-        url: '/data/tracks.json',
+        url: 'http://senchalearn.github.com/Nested-list-demo/data/tracks.json',
         reader: {
             type: 'tree',
             root: 'items'
