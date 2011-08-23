1632eb9c66fa92ead5b28b5f9535c15cf6ef1e40
Drew Neil <andrew.jr.neil@gmail.com>
2011-08-23 17:22:26 +0100

    Create URLs: /data/{artists|albums|tracks}.json

--- application.rb
+++ application.rb
@@ -4,6 +4,23 @@ require 'sinatra'
 require 'active_record'
 require File.join(File.dirname(__FILE__), 'environment')
 
+mime_type :json, 'application/json'
+
 get '/' do
   erb :index
+end
+
+get '/data/artists.json' do
+  content_type :json
+  JSON.pretty_generate(Catalogue.items(:leaf => :artist))
+end
+
+get '/data/albums.json' do
+  content_type :json
+  JSON.pretty_generate(Catalogue.items(:leaf => :album))
+end
+
+get '/data/tracks.json' do
+  content_type :json
+  JSON.pretty_generate(Catalogue.items(:leaf => :track))
 end
