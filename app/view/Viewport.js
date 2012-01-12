521914a5f0ee051ae64fdc8251f061f349dfd01b
Drew Neil <andrew.jr.neil@gmail.com>
2012-01-12 10:26:31 +0100

    Make NestedList render.

--- app/view/Viewport.js
+++ app/view/Viewport.js
@@ -6,36 +6,36 @@ Ext.define('NestedListDemo.view.Viewport', {
         fullscreen: true,
         title: 'Music',
         store: Ext.create('NestedListDemo.store.MusicStore'),
+        getDetailCard: function(item, parent) {
+            var itemData = item.attributes.record.data,
+            parentData = parent.attributes.record.data,
+            detailCard = new Ext.Panel({
+                scroll: 'vertical',
+                styleHtmlContent: true,
+                tpl: ["<h2>{text}</h2>","{info}"]
+            });
+            detailCard.update(itemData);
+            this.backButton.setText(parentData.text);
+            return detailCard;
+        },
+        getItemTextTpl: function() {
+            var tplConstructor = '{text}' +
+                '<tpl if="model === \'Artist\'">'+
+                    '<div class="metadata">' +
+                        ' {[values.items.length]} albums' +
+                    '</div>' +
+                '</tpl>' +
+                '<tpl if="model === \'Album\'">'+
+                    '<div class="metadata">' +
+                        ' {[values.items.length]} tracks' +
+                    '</div>' +
+                '</tpl>' +
+                '<tpl if="model === \'Track\'">'+
+                    '<div class="metadata">' +
+                        ' Duration: {[Math.floor(values.duration/60)]}:{[values.duration%60]}' +
+                    '</div>' +
+                '</tpl>';
+            return tplConstructor;
+        }
     },
-    getDetailCard: function(item, parent) {
-        var itemData = item.attributes.record.data,
-        parentData = parent.attributes.record.data,
-        detailCard = new Ext.Panel({
-            scroll: 'vertical',
-            styleHtmlContent: true,
-            tpl: ["<h2>{text}</h2>","{info}"]
-        });
-        detailCard.update(itemData);
-        this.backButton.setText(parentData.text);
-        return detailCard;
-    },
-    getItemTextTpl: function() {
-        var tplConstructor = '{text}' +
-            '<tpl if="model === \'Artist\'">'+
-                '<div class="metadata">' +
-                    ' {[values.items.length]} albums' +
-                '</div>' +
-            '</tpl>' +
-            '<tpl if="model === \'Album\'">'+
-                '<div class="metadata">' +
-                    ' {[values.items.length]} tracks' +
-                '</div>' +
-            '</tpl>' +
-            '<tpl if="model === \'Track\'">'+
-                '<div class="metadata">' +
-                    ' Duration: {[Math.floor(values.duration/60)]}:{[values.duration%60]}' +
-                '</div>' +
-            '</tpl>';
-        return tplConstructor;
-    }
 });
