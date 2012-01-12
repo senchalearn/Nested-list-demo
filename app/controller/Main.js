e96c311eccf3717511c9d630848af67ccff97dfe
Drew Neil <andrew.jr.neil@gmail.com>
2012-01-12 11:39:15 +0100

    Update detailCard when leaf node is clicked.

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -2,15 +2,30 @@ Ext.define('NestedListDemo.controller.Main', {
     extend: 'Ext.app.Controller',
     
     models: ['ListItem'],
-    views: ['Viewport'],
+    views:  ['Viewport'],
     
     refs: [
         {
-            ref: 'viewport',
-            selector: '#rootpanel'
+            ref:       'viewport',
+            selector:  '#rootpanel'
         }
     ],
     
-    init: function() {},
+    init: function() {
+        this.control({
+            'nestedlist': {
+                leafitemtap: 'updateDetailCard'
+            }
+        })
+    },
+
+    updateDetailCard: function(list, index, element, event, options) {
+        var nestedList = this.getViewport(),
+            detailCard = nestedList.getDetailCard(),
+            store      = list.getStore(),
+            item       = store.getAt(index);
+
+        detailCard.setData(item.data);
+    }
 
 });
