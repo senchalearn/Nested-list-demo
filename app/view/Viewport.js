e2145c9231aabfeb2f74991a10603166a68748d0
Drew Neil <andrew.jr.neil@gmail.com>
2012-01-12 11:51:52 +0100

    Fix getItemTextTpl() to include metadata.

--- app/view/Viewport.js
+++ app/view/Viewport.js
@@ -11,24 +11,24 @@ Ext.define('NestedListDemo.view.Viewport', {
             styleHtmlContent: true,
             tpl: ["<h2>{text}</h2>","{info}"]
         },
-        getItemTextTpl: function() {
-            var tplConstructor = '{text}' +
-                '<tpl if="model === \'Artist\'">'+
-                    '<div class="metadata">' +
-                        ' {[values.items.length]} albums' +
-                    '</div>' +
-                '</tpl>' +
-                '<tpl if="model === \'Album\'">'+
-                    '<div class="metadata">' +
-                        ' {[values.items.length]} tracks' +
-                    '</div>' +
-                '</tpl>' +
-                '<tpl if="model === \'Track\'">'+
-                    '<div class="metadata">' +
-                        ' Duration: {[Math.floor(values.duration/60)]}:{[values.duration%60]}' +
-                    '</div>' +
-                '</tpl>';
-            return tplConstructor;
-        }
     },
+    getItemTextTpl: function() {
+        var tplConstructor = '{text}' +
+            '<tpl if="model === \'Artist\'">' +
+            '    <div class="metadata">' +
+            '         {[values.items.length]} albums' +
+            '    </div>' +
+            '</tpl>' +
+            '<tpl if="model === \'Album\'">'+
+            '    <div class="metadata">' +
+            '         {[values.items.length]} tracks' +
+            '    </div>' +
+            '</tpl>' +
+            '<tpl if="model === \'Track\'">'+
+            '    <div class="metadata">' +
+            '         Duration: {[Math.floor(values.duration/60)]}:{[values.duration%60]}' +
+            '    </div>' +
+            '</tpl>';
+        return tplConstructor;
+    }
 });
